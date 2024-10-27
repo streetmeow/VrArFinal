f745aa0bcc6d5f437da89ce148b5d0a16ffb255e
streetmeow <chojh106@gmail.com>
2024-10-27 20:17:46 +0900

    일단 백업

---
+++ Assets/Scripts/InputMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InputMovement : MonoBehaviour
+{
+    [SerializeField] private InputActionReference reference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Vector2 axes = reference.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
