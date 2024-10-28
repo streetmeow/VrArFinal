b28f2b7a7d883bbeae3790bc8643f2fb5da24130
streetmeow <chojh106@gmail.com>
2024-10-28 16:04:49 +0900

    오브젝트 잡기 기능 추가

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrabObject : MonoBehaviour
+{
+    public Transform target;
+    public InputActionReference reference;
+    public Rigidbody rb;
+    private bool grabbed = false;
+    private Vector3 currentPos;
+    private Vector3 movePos;
+    
+    private void Awake()
+    {
+        reference.action.Enable();
+        reference.action.performed += GrabObjectVoid;
+    }
+
+    private void Update()
+    {
+        if (grabbed)
+        {
+            gameObject.transform.position = target.position + new Vector3(0.1f, 0.1f, 0.1f);
+            movePos = gameObject.transform.position - currentPos;
+            currentPos = gameObject.transform.position;
+        }
+    }
+
+    private void GrabObjectVoid(InputAction.CallbackContext context)
+    {
+        if (Vector3.Distance(target.position, gameObject.transform.position) < 0.2f && grabbed == false)
+        {
+            grabbed = true;
+            currentPos = gameObject.transform.position;
+        } else if (grabbed)
+        {
+            grabbed = false;
+            Invoke("Throw", 0.01f);
+        }
+    }
+
+    private void Throw()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(movePos) * 10, ForceMode.Impulse);
+    }
+}
